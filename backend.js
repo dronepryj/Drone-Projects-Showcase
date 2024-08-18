631e4302a733ad03123af76ffbe5218415e31932
dronepryj <drone.pryj@gmail.com>
2024-08-18 15:15:04 +0530

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,5 +1,3 @@
-// backend.js
-
 // Authentication Check
 function checkAuthentication() {
     if (!sessionStorage.getItem('loggedIn')) {
@@ -53,23 +51,3 @@ function showProject(projectId) {
         }
     }
 }
-
-// Event listener for login form submission (for login.html page)
-document.getElementById('loginForm').addEventListener('submit', function(e) {
-    e.preventDefault(); // Prevent form from submitting normally
-
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    // Hardcoded credentials
-    const validUsername = 'admin';
-    const validPassword = 'password123';
-
-    if (username === validUsername && password === validPassword) {
-        sessionStorage.setItem('loggedIn', true); // Set login status in session storage
-        window.location.href = 'index.html'; // Redirect to main page after successful login
-    } else {
-        // Display error message
-        document.getElementById('error-message').textContent = 'Invalid username or password';
-    }
-});
